51bbab7958bf2632ff8d3cd1f571fea0cb1da26b
proxin187 <thugshaker627@gmail.com>
2025-05-17 23:09:04 +0200

    wip: component macro

--- packages/saar_components/src/lib.rs
+++ packages/saar_components/src/lib.rs
@@ -1,8 +1,15 @@
-use saar::component::{Component, Callback, Context};
+use saar::component::{Component, ComponentRef, Callback, Context};
 use saar::html::Html;
 
 // TODO: create a macro that automatically generates a template for all the default html components
 
+
+macro_rules! component {
+    ($name:ident, $tag:expr) => {
+    }
+}
+
+component!(H1, "h1");
 
 pub struct Message;
 
