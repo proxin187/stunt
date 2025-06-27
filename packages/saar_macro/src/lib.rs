753544f824ad3d24abc9da11c5f7b9be40009239
proxin187 <thugshaker627@gmail.com>
2025-06-27 22:36:48 +0200

    wip: started working on the proc macro

--- packages/saar_macro/src/lib.rs
+++ packages/saar_macro/src/lib.rs
@@ -1,4 +1,30 @@
+use proc_macro::TokenStream;
+
+use syn::parse::{Parse, ParseStream, Result};
+use syn::{Token, Ident, Expr};
 
 
+struct Attribute {
+    name: Ident,
+    value: Expr,
+}
+
+impl Parse for Attribute {
+    fn parse(input: ParseStream) -> Result<Attribute> {
+        Ok(Attribute {
+            name: input.parse(),
+            value: input.parse(),
+        })
+    }
+}
+
+struct Tag {
+    name: Ident,
+    attributes: Vec<Attribute>,
+}
+
+#[proc_macro]
+pub fn html(tokens: TokenStream) {
+}
 
 
