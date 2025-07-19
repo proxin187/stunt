6db704b20b7a97517538be4486f1a283c6e34c92
proxin187 <thugshaker627@gmail.com>
2025-07-19 12:38:04 +0200

    wip: started work on properties

--- packages/puri_core/src/component/mod.rs
+++ packages/puri_core/src/component/mod.rs
@@ -4,12 +4,14 @@ pub mod tree;
 use tree::{Tree, Props, Attributes};
 use state::Identity;
 
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::any::Any;
 
 
 pub trait Component {
     type Message: 'static;
+    type Properties: Properties;
 
     fn create() -> Self where Self: Sized;
 
@@ -28,6 +30,10 @@ impl<T: Component> BaseComponent for T {
     fn base_callback(&mut self, callback: &Arc<dyn Any + Send + Sync>) { T::callback(self, callback.downcast_ref().expect("invalid callback type")) }
 
     fn base_view(&self, ctx: Context) -> Tree { T::view(self, ctx) }
+}
+
+pub trait Properties {
+    fn create(attr: HashMap<String, Arc<dyn Any>>) -> Self where Self: Sized;
 }
 
 pub struct Context {
--- packages/puri_router/src/lib.rs
+++ packages/puri_router/src/lib.rs
@@ -1,4 +1,32 @@
+use puri::prelude::*;
 
 
+pub struct Properties {
+}
+
+pub struct Route {
+}
+
+impl Component for Route {
+    type Message = ();
+
+    fn create() -> Route {
+        Route {}
+    }
+
+    fn callback(&mut self, _message: &()) {}
+
+    fn view(&self, ctx: Context) -> Tree {
+        let window = web_sys::window()
+            .expect("no window found")
+            .location()
+            .pathname()
+            .expect("failed to get pathname");
+
+        html! {
+            <h1></h1>
+        }
+    }
+}
 
 
